455e154f1b3cbed889ae67f48a9fbe35b2dd1e6b
Brianna Kemp <kemp.bri@gmail.com>
2020-04-30 11:10:08 -0700

    Implemented bipartition method

--- lib/possible_bipartition.rb
+++ lib/possible_bipartition.rb
@@ -1,4 +1,35 @@
+def dfs(dislikes, vertex, tracker, groups)
+  return groups if !vertex
+  
+  tracker[vertex] = true;
+  currColor = groups[vertex]
+  
+  dislikes[vertex].each do |adjNode|  
+    if !tracker[adjNode]
+      if currColor == "A"
+        groups[adjNode] = "B"
+      else
+        groups[adjNode] = "A"
+      end
+      dfs(dislikes, adjNode, tracker, groups)
+    else
+      return false if currColor == groups[adjNode]
+    end
+  end
+  
+  return groups
+  
+  
+end
 
 def possible_bipartition(dislikes)
-  raise NotImplementedError, "possible_bipartition isn't implemented yet"
+  return true if dislikes.length == 0
+  
+  dislikes.each do |k,v|
+    groups = {}
+    groups[k] = "A"
+    return false if dfs(dislikes, k, {}, groups) == false
+  end
+  
+  return true
 end
